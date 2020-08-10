4b5391b137fdfc16b0be503aab417da37c423b19
Omar Minaya <omiinaya@gmail.com>
2020-08-10 05:28:41 -0400

    functions for testing.

--- scripts.js
+++ scripts.js
@@ -1,4 +1,24 @@
+//changes color of 1-1
 function gridTest() {
     console.log("Test");
     document.getElementById("cell-1-1").style.backgroundColor = "black";
 }
+
+function gridTest2() {
+    console.log("Test 2");
+    document.addEventListener('click', function(e) {
+        var clicked = e.target.id
+        console.log(clicked);
+        document.getElementById(clicked).style.backgroundColor = "black";
+
+    }, false);
+
+}
+
+//logs id of clicked cell
+function clickTest() {
+    document.addEventListener('click', function(e) {
+        console.log( e.target.id );
+    }, false);     
+}
+
