c063f844cf172cca5521964843f2cecb6d34f2a1
Omar Minaya <omiinaya@gmail.com>
2020-08-10 05:51:53 -0400

    more successful tests.

--- scripts.js
+++ scripts.js
@@ -6,18 +6,55 @@ function test1() {
 
 //logs id of clicked cell.
 function test2() {
-    document.addEventListener('click', function(e) {
-        console.log( e.target.id );
-    }, false);     
+    console.log("Test 2");
+    document.addEventListener('click', function (e) {
+        console.log(e.target.id);
+    }, false);
 }
 
 //changes color of clicked cell.
 function test3() {
-    console.log("Test 2");
-    document.addEventListener('click', function(e) {
+    console.log("Test 3");
+    document.addEventListener('click', function (e) {
         var clicked = e.target.id
         console.log(clicked);
         document.getElementById(clicked).style.backgroundColor = "black";
     }, false);
 }
 
+//logs id of hovered over cell.
+function test4() {
+    console.log("Test 4");
+    document.addEventListener('mouseover', function (e) {
+        var current = e.target.id
+        console.log(current);
+    }, false);
+}
+
+//logs id of hovered over cell only while clicked.
+function test5() {
+    var mouseDown = false;
+    window.addEventListener('mousedown', function () { mouseDown = true })
+    window.addEventListener('mouseup', function () { mouseDown = false })
+    window.addEventListener('mousemove', function (e) {
+        if (!mouseDown) {
+            return;
+        }
+        console.log(e.target.id);
+    })
+}
+
+//changes color of hovered over cell while mouse is down.
+function test6() {
+    var mouseDown = false;
+    window.addEventListener('mousedown', function () { mouseDown = true })
+    window.addEventListener('mouseup', function () { mouseDown = false })
+    window.addEventListener('mousemove', function (e) {
+        if (!mouseDown) {
+            return;
+        }
+        var current = e.target.id;
+        console.log(current);
+        document.getElementById(current).style.backgroundColor = "black";
+    })
+}
