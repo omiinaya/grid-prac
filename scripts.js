273ace70571b6cdb39b990380fdb2fbb838a1c7f
Omar Minaya <omiinaya@gmail.com>
2020-08-10 10:18:40 -0400

    successfully changing colors when touching another color.

--- scripts.js
+++ scripts.js
@@ -78,38 +78,77 @@ function test7() {
         }
         var current = e.target.id;
         var elem = document.getElementById(current);
-        var currentColor= window.getComputedStyle(elem, null).getPropertyValue("background-color");
+        var currentColor = window.getComputedStyle(elem, null).getPropertyValue("background-color");
         console.log(currentColor);
+    })
+}
+
+function test8() {
+    console.log("Test 8");
+    document.getElementById("cell-3-4").style.backgroundColor = "rgb(0,0,0)";
+    document.getElementById("cell-3-6").style.backgroundColor = "rgb(0,128,0)";
+
+    var mouseDown = false;
+    var initialId;
+    var initialElem;
+    var initialColor;
+    var gray = "rgb(128, 128, 128)";
+    var green = "rgb(0, 128, 0)";
+    window.addEventListener('mousedown', function (e) {
+        initialId = e.target.id;
+        initialElem = document.getElementById(initialId);
+        initialColor = window.getComputedStyle(initialElem, null).getPropertyValue("background-color");
+        mouseDown = true
+    })
+    window.addEventListener('mouseup', function () {
+        mouseDown = false
+    })
+    window.addEventListener('mousemove', function (e) {
+        if (!mouseDown) {
+            return;
+        }
+        var currentId = e.target.id;
+        var currentElem = document.getElementById(currentId);
+        var currentColor = window.getComputedStyle(currentElem, null).getPropertyValue("background-color");
+        console.log("current color: " + currentColor);
+        console.log(gray);
+
+        if (currentColor == green) {
+            initialColor = currentColor;
+            document.getElementById(currentId).style.backgroundColor = currentColor;
+        } else {
+            document.getElementById(currentId).style.backgroundColor = initialColor;
+        }
     })
 }
 
 //code start
 function start() {
-    console.log("starting scripts.");
-    generateGrid()
-    loadLevel()
-}
+            console.log("starting scripts.");
+            generateGrid()
+            loadLevel()
+        }
 
 function generateGrid() {
-    console.log("generating grid.");
-    for (var i = 0; i < gridSize; i++) {
-        const div = document.createElement('div');
-        div.setAttribute("id","row-"+i)
-        div.innerHTML = `
-            <div id="cell-`+i+`-1" class="cell"></div>
-            <div id="cell-`+i+`-2" class="cell"></div>
-            <div id="cell-`+i+`-3" class="cell"></div>
-            <div id="cell-`+i+`-4" class="cell"></div>
-            <div id="cell-`+i+`-5" class="cell"></div>
-            <div id="cell-`+i+`-6" class="cell"></div>
-            <div id="cell-`+i+`-7" class="cell"></div>
-            <div id="cell-`+i+`-8" class="cell"></div>
+            console.log("generating grid.");
+            for (var i = 0; i < gridSize; i++) {
+                const div = document.createElement('div');
+                div.setAttribute("id", "row-" + i)
+                div.innerHTML = `
+            <div id="cell-`+ i + `-1" class="cell"></div>
+            <div id="cell-`+ i + `-2" class="cell"></div>
+            <div id="cell-`+ i + `-3" class="cell"></div>
+            <div id="cell-`+ i + `-4" class="cell"></div>
+            <div id="cell-`+ i + `-5" class="cell"></div>
+            <div id="cell-`+ i + `-6" class="cell"></div>
+            <div id="cell-`+ i + `-7" class="cell"></div>
+            <div id="cell-`+ i + `-8" class="cell"></div>
         `;
-        document.getElementById('row').appendChild(div);
-    }
-    console.log("grid finished generated.");
-}
+                document.getElementById('row').appendChild(div);
+            }
+            console.log("grid finished generated.");
+        }
 
 function loadLevel() {
-    console.log("no levels to load.");
-}
+            console.log("no levels to load.");
+        }
