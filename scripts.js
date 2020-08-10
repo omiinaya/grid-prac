5fedc0f8a6ce3cada2f8c06d66d34fe39410874f
Omar Minaya <omiinaya@gmail.com>
2020-08-10 09:02:15 -0400

    dynamically generating grid.

--- scripts.js
+++ scripts.js
@@ -1,7 +1,17 @@
+////////////////////
+//global variables//
+////////////////////
+
+var gridSize = 8;
+
+/////////////////////////
+//functions for testing//
+/////////////////////////
+
 //changes color of 1-1
 function test1() {
     console.log("Test");
-    document.getElementById("cell-1-1").style.backgroundColor = "black";
+    document.getElementById("cell-0-1").style.backgroundColor = "black";
 }
 
 //logs id of clicked cell.
@@ -59,9 +69,10 @@ function test6() {
     })
 }
 
+//changes color of first tile and logs color of current hovered cell. (except it doesnt for some reaason...)
 function test7() {
     console.log("Test 7");
-    document.getElementById("cell-1-1").style.backgroundColor = "black";
+    document.getElementById("cell-0-1").style.backgroundColor = "black";
 
     var mouseDown = false;
     window.addEventListener('mousedown', function () { mouseDown = true })
@@ -74,4 +85,30 @@ function test7() {
         var currentColor = document.getElementById(current).style.backgroundColor;
         console.log(currentColor);
     })
+}
+
+//code start
+function start() {
+    console.log("starting scripts.");
+    generateGrid()
+}
+
+function generateGrid() {
+    console.log("generating grid.");
+    for (var i = 0; i < gridSize; i++) {
+        const div = document.createElement('div');
+        div.setAttribute("id","row-"+i)
+        div.innerHTML = `
+            <div id="cell-`+i+`-1" class="cell"></div>
+            <div id="cell-`+i+`-2" class="cell"></div>
+            <div id="cell-`+i+`-3" class="cell"></div>
+            <div id="cell-`+i+`-4" class="cell"></div>
+            <div id="cell-`+i+`-5" class="cell"></div>
+            <div id="cell-`+i+`-6" class="cell"></div>
+            <div id="cell-`+i+`-7" class="cell"></div>
+            <div id="cell-`+i+`-8" class="cell"></div>
+        `;
+        document.getElementById('row').appendChild(div);
+    }
+    console.log("grid finished generated.");
 }
