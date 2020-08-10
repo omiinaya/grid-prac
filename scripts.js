a0bc0a428b8fe05d3d4963c04f08f8193eb327ba
Omar Minaya <omiinaya@gmail.com>
2020-08-10 05:30:28 -0400

    re-organized.

--- scripts.js
+++ scripts.js
@@ -1,24 +1,23 @@
 //changes color of 1-1
-function gridTest() {
+function test1() {
     console.log("Test");
     document.getElementById("cell-1-1").style.backgroundColor = "black";
 }
 
-function gridTest2() {
+//logs id of clicked cell.
+function test2() {
+    document.addEventListener('click', function(e) {
+        console.log( e.target.id );
+    }, false);     
+}
+
+//changes color of clicked cell.
+function test3() {
     console.log("Test 2");
     document.addEventListener('click', function(e) {
         var clicked = e.target.id
         console.log(clicked);
         document.getElementById(clicked).style.backgroundColor = "black";
-
     }, false);
-
-}
-
-//logs id of clicked cell
-function clickTest() {
-    document.addEventListener('click', function(e) {
-        console.log( e.target.id );
-    }, false);     
 }
 
